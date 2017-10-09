9ee1b606fea359d0fd6081d40b6cb2013bbc1ee6
Unknown <luke.r872@stu.nebo.edu>
2017-10-09 12:40:05 -0600

    Added scripts and played with player and floor

---
+++ Ghost House/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove3D : MonoBehaviour {
+	public float moveSpeed;
+	public float turnSpeed;
+	public float jumpHeight;
+	
+	}
+	
+	
+	void Update () {
+		var j = Input.GetAxis("Jump")* Time.deltaTime * jumpHeight
+		var y = Input.GetAxis("Horizontal")* Time.deltaTime * turnSpeed;
+		var z = Input.GetAxis("Vertical")* Time.deltaTime * moveSpeed;
+
+		transform.Rotate(0,y,0);
+		transform.Translate(0,0,z);
+		transform.Translate(0,j,0);
+	}
+}
