fcb837aa8aaa7d569deb32cf77d80df1bbd2ad47
Unknown <luke.r872@stu.nebo.edu>
2017-11-16 13:02:42 -0700

    edited ghost stun script

--- Ghost House/Assets/Scripts/GhostStun.cs
+++ Ghost House/Assets/Scripts/GhostStun.cs
@@ -27,12 +27,12 @@ public class GhostStun : MonoBehaviour {
 			print("Ghost is Stunned!");
 
 			other.GetComponent<GhostAI>().moveSpeed = 0f;
-			StartCoroutine(Wait(5));
+			StartCoroutine(Wait(5, other));
 		}
 	}
-	IEnumerator Wait(float time){
+	IEnumerator Wait(float time, Collider other){
 		yield return new WaitForSeconds(time);
-		ghost.GetComponent<GhostAI>().moveSpeed = 5f;
+		other.gameObject.GetComponent<GhostAI>().moveSpeed = 5f;
 		print("Ghost is unstunned!");
 	
 		}
