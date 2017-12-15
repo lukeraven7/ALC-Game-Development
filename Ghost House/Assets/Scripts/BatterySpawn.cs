880d2480ddf9a8fc89a36d6b6a244a9fc577fcbd
Unknown <luke.r872@stu.nebo.edu>
2017-12-15 13:09:24 -0700

    Added battery spawn script

---
+++ Ghost House/Assets/Scripts/BatterySpawn.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BatterySpawn : MonoBehaviour {
+
+	public Rigidbody battery;
+
+	public float spawnTime;
+
+	public bool batSpawned;
+
+	public Transform spawnPoint;
+
+	// Use this for initialization
+	void Start () {
+		batSpawned = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if(batSpawned == false){
+			StartCoroutine(SpawnBat(spawnTime, battery));
+		}
+		else{
+			print("There is already a battery!");
+		}
+	}
+	void OnTriggerEnter(Collider other){
+		if(other.Rigidbody.tag == "Battery"){
+			batSpawned = true;
+		}
+		else{
+			batSpawned = false;
+		}
+	}
+
+	IEnumerator SpawnBat(float time, Rigidbody bat){
+		yield return new WaitForSeconds(time);
+		Instantiate(battery, spawnPoint.position, spawnPoint.rotation);
+		batSpawned = true;
+	}
+
+}
